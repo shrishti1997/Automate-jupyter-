627473aa967905a612af95a2a920a74bfcde6eaa
Shrishti Sharma <48645595+shrishti1997@users.noreply.github.com>
2020-04-06 23:23:35 +0530

    Add files via upload

---
+++ automate_jupyter.py
@@ -0,0 +1,14 @@
+import pyautogui as a
+import time
+a.moveTo(123,879,4)
+a.click(123,879)
+a.typewrite('anaconda',0.5)
+a.press('enter')
+time.sleep(15)
+a.moveTo(618,437,4)
+a.click(618,437)
+a.moveTo(1208,196)
+a.click(1208,196)
+a.moveTo(1112,244)
+
+
